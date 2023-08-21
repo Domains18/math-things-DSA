27252ccfd838fc860a1439634686f69e085c1b08
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-08-21 10:22:53 +0300

    proto-c auth typescript

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AuthModule {}
